86b9fb7f6f9f5f4155a1c70a38dfc0dcc73ced99
simoarajarvi <7988758+simoarajarvi@users.noreply.github.com>
2023-10-17 16:46:55 -0700

    Update factorials.R
    Update to read from the files.

--- factorials.R
+++ factorials.R
@@ -1,7 +1,4 @@
-# Install and load necessary packages
-install.packages("FrF2")
 
-# Installing and loading the FrF2 package
 if (!requireNamespace("FrF2", quietly = TRUE)) {
   install.packages("FrF2")
 }
@@ -17,10 +14,10 @@ frac_design <- FrF2(nfactors = 7, resolution = 4, randomize = TRUE, factor.names
 # View the design
 print(frac_design)
 
-## results <- read.csv("data/experiment.csv")
+results <- read.csv("data/experiment.csv")
 
 ## Just generate some data to validate..
 #frac_design$Y <- rnorm(nrow(frac_design2))
-set.seed(111)
-frac_design$Y <- rnorm(nrow(frac_design))
+# set.seed(111)
+# frac_design$Y <- rnorm(nrow(frac_design))
 summarylm(Y ~ ., data = frac_design))
